b474173f803592f929dc175cd50247a1ad34c2a6
VipulThapliyal <vipulthapliyal122@gmail.com>
2023-07-25 15:22:36 +0530

    259 Done Store Data in SQL DataBase

--- lib/providers/user_places.dart
+++ lib/providers/user_places.dart
@@ -1,8 +1,13 @@
 import 'dart:io';
 
 import 'package:favorite_place_app_section_13/models/place.dart';
+
 import 'package:path_provider/path_provider.dart' as syspaths;
 import 'package:path/path.dart' as path;
+
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:sqflite/sqlite_api.dart';
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class UserPlacesNotifier extends StateNotifier<List<Place>> {
@@ -17,6 +22,23 @@ class UserPlacesNotifier extends StateNotifier<List<Place>> {
       title: title,
       image: copiedImage
     );
+
+    final dbPath = await sql.getDatabasesPath();
+    final db = await sql.openDatabase(
+      path.join(
+        dbPath, "places.db"
+      ),
+      onCreate: (db, version) {
+        return db.execute("CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT)");
+      },
+      version: 1,
+    );
+
+    db.insert("user_places", {
+      "id": newPlace.id,
+      "title": newPlace.title,
+      "image": newPlace.image.path,
+    });
 
     state = [newPlace, ...state];
   }
